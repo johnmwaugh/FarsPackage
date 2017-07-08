0d06936b706a47d4486e3f3465ce890796d98e50
johnmwaugh <johnmwaugh@hotmail.com>
2017-07-08 12:07:36 +0100

    raw data

--- data-raw/read_raw_data.R
+++ data-raw/read_raw_data.R
@@ -3,3 +3,5 @@ library(readr)
 readr::read_csv("accident_2015.csv.bz2", progress = FALSE)
 readr::read_csv("accident_2014.csv.bz2", progress = FALSE)
 readr::read_csv("accident_2013.csv.bz2", progress = FALSE)
+
+
